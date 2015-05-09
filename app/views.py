a486e02508b4f88b693ac90d3b90595333a2dbd7
Omar Arguelles Arreola <omar00231@gmail.com>
2015-05-09 01:57:26 -0600

    Views: Now you will have a welcome messsage rendering your first template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello!!'
+    user = {'nickname': 'Gabriela'}
+    posts = [
+        {
+            'author': {'nickname': 'bragerson'},
+            'body': 'Beautiful day in the beach babies'
+        },
+        {
+            'author': {'nickname': 'andersonsss'},
+            'body': 'The magic things of the big trick'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
