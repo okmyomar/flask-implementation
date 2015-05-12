7de6dfb794ab1c80e3816f96ad32a50753938da9
Omar Arguelles Arreola <omar00231@gmail.com>
2015-05-11 22:22:09 -0600

    App: Add support for opneid and create openid list

--- app/views.py
+++ app/views.py
@@ -1,18 +1,32 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'Gabriela'}
-    posts = [
-        {
-            'author': {'nickname': 'bragerson'},
-            'body': 'Beautiful day in the beach babies'
-        },
-        {
-            'author': {'nickname': 'andersonsss'},
-            'body': 'The magic things of the big trick'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     user = {'nickname': 'Gabriela'}
+#     posts = [
+#         {
+#             'author': {'nickname': 'bragerson'},
+#             'body': 'Beautiful day in the beach babies'
+#         },
+#         {
+#             'author': {'nickname': 'andersonsss'},
+#             'body': 'The magic things of the big trick'
+#         }
+#     ]
+#     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('You are in man!! OpenId=%s, remember_me=%s' %
+            (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+
+    return render_template('login.html',
+                            title='Sign in',
+                            form=form,
+                            providers=app.config['OPENID_PROVIDERS'])
