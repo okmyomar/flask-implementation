1754a559076e9b48f718e4613b5e0bcc56cfd24a
Omar Arguelles Arreola <omar00231@gmail.com>
2015-05-13 22:29:26 -0600

    Database: Adding support to make database migrations, upgrade and downgrade

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
-app.config.from_object('app.config')
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
